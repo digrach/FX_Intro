537fc15bb1c10000ac0fd57c48891a6c3a7a42be
unknown <ColleyR@NOTEB-043738.chisholm.edu.au>
2014-04-02 16:08:50 +1100

    Add main

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,14 +6,22 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
+// Subclass of JavaFX Application.
 public class Main extends Application {
+	
+	// Overrides start using the default stage.
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			// Layout pane.
 			BorderPane root = new BorderPane();
+			// Set the root layout of the scene.
 			Scene scene = new Scene(root,400,400);
+			// Default styling of the scene.
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// Set the stage's scene.
 			primaryStage.setScene(scene);
+			// Show the stage.
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -21,6 +29,8 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		// Main points to Application launch method.
 		launch(args);
 	}
 }
+
